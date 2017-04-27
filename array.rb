c7cd475d1734c060b3ee50e1b8f524fc3769d445
SiamKing <thesiameseelectric@gmail.com>
2017-04-27 15:51:30 -0700

    refactored

--- array.rb
+++ array.rb
@@ -1,12 +1,4 @@
 def makeArrayConsecutive2(statues)
-    statues.sort!
-    c = 0
-    i = 0
-    while i < statues.length - 1 do
-        if statues[i+1] != statues[i] + 1
-            c += statues[i+1] - statues[i] - 1
-        end
-        i+=1
-    end
-    c
+    arr = (statues.min..statues.max).to_a
+    (arr - statues).length
 end
